046f27c5d21bd72585fb1e849d5e76234cc5f2e1
PrasadSatpute <prasadsatpute000@gmail.com>
2021-09-16 22:31:08 +0530

    IntroPage

--- Portfolio/portfolio/src/App.js
+++ Portfolio/portfolio/src/App.js
@@ -1,15 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './Components/NavBar/Nav';
+import IntroPage from './Components/IntroPage/IntroPage';
 
 function App() {
   return (
     <div className="App">
-      <div className='cursor'></div>
-      <div className='cursor2'></div>
+      {/* <div className='cursor'></div>
+      <div className='cursor2'></div> */}
         <div>
           <Nav></Nav>
         </div>
+
+        <div className='introPage'>
+          <IntroPage></IntroPage>
+        </div>
+
     </div>
   );
 }
---
+++ Portfolio/portfolio/src/Components/IntroPage/IntroPage.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./IntroPage.css"
+
+const IntroPage = () => {
+    return(
+        <div className='introPageWraper'>
+            <h5>Hello My Name Is</h5>
+            <h1>Prasad Satpute</h1>
+            <h4>And I'm WebDeveloper</h4>
+        </div>
+    )
+}
+
+export default IntroPage;
