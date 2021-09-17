427bb3aa961afe128c0b4d5a2c98e3610e27269d
PrasadSatpute <prasadsatpute000@gmail.com>
2021-09-17 07:45:09 +0530

    HireMe Modal

--- Portfolio/portfolio/src/Components/IntroPage/IntroPage.js
+++ Portfolio/portfolio/src/Components/IntroPage/IntroPage.js
@@ -7,7 +7,31 @@ const IntroPage = () => {
             <h5>Hello My Name Is</h5>
             <h1>Prasad Satpute</h1>
             <h4>And I'm WebDeveloper</h4>
-            <button className='btn btn-outline-success my-2 my-sm-0'>Hire Me</button>
+            
+            <button type="button" className="btn btn-outline-success" data-toggle="modal" data-target="#exampleModal">
+            Hire Me
+            </button>
+
+
+            <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        ...
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-outline-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" className="btn btn-outline-success">Save changes</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
