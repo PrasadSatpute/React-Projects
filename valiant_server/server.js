5f432fcd2a0f17cfa188ef1f4a155c0047343a29
PrasadSatpute <prasadsatpute000@gmail.com>
2021-10-25 15:36:21 +0530

    server addcustomer and getcustomers

---
+++ valiant_server/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(cors('*'))
+app.use(bodyParser.json())
+
+// routers
+const routerUser = require('./routers/user')
+
+// add the routers
+app.use('/user', routerUser)
+
+// static routing
+// any file in uploads directory does not require any route
+// express will simply return any file from uploads directory
+// app.use(express.static('uploads'))
+
+app.listen(3300, '0.0.0.0', () => {
+  console.log('server started on port 3300')
+})
