ad2f1660c18389149088a031373b9b4af67a44c7
PrasadSatpute <prasadsatpute000@gmail.com>
2021-11-05 13:29:58 +0530

    PreeLoader

--- valiant/src/NavBar/NavBar.js
+++ valiant/src/NavBar/NavBar.js
@@ -7,17 +7,20 @@ const NavBar = () => {
     return(
         <nav>
             <div className='NavBarLogo'><h4>VALIANT</h4></div>
+            <div className='NavBarLogo'><h4>LOGO</h4></div>
             <div class="hamburger">
                 <div class="line"></div>
                 <div class="line"></div>
                 <div class="line"></div>
             </div>
             <ul class="nav-links">
-                <li><a href="#About">About</a></li>
+                <li><a href="#About">ABOUT</a></li>
                 
-                <li><a href="#Project">Project</a></li>
+                <li><a href="#Project">PROJECTS</a></li>
+                
+                <li><a href="#Project">ARCHITECTS</a></li>
 
-                <li><a href="#Contact">Contact</a></li>
+                <li><a href="#Contact">CONTACT</a></li>
             </ul>
         </nav>
     )
