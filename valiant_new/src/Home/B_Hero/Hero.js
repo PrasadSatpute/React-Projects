e63c99d944c8f066dfe88c0ba6b18a894e1c7f45
PrasadSatpute <prasadsatpute000@gmail.com>
2021-11-07 09:16:17 +0530

    New Template

---
+++ valiant_new/src/Home/B_Hero/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Hero = () => {
+    return(
+        <section id="hero">
+            <div className="hero-container">
+            <h1>Welcome to ULFA</h1>
+            <h2>We are team of talanted designers making EVENTS</h2>
+            <a href="#about" className="btn-get-started">Book Event</a>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
