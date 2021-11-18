4a63ab5ba0c13ffa9b8b85ee7bef7fcb296d93e4
PrasadSatpute <prasadsatpute000@gmail.com>
2021-11-18 09:59:14 +0530

    TypeText

--- valiant_new/src/Home/B_Hero/Hero.js
+++ valiant_new/src/Home/B_Hero/Hero.js
@@ -8,6 +8,9 @@ const Hero = () => {
             <h1>Welcome to VALIANT</h1>
             <h2>REAL-ESTATE AND CONSTRUCTIONS</h2>
             <h3>We are team of talanted designers making Great things</h3>
+            <h3 class="typewrite" data-period="2000" data-type='[ "Welcome to VALIANT", "We are Creative.", "We Love Design.", "We Love to Develop." ]'>
+                <span class="wrap"></span>
+            </h3>
             <a href="#about" className="btn-get-started">EXPLORE US</a>
             </div>
         </section>
